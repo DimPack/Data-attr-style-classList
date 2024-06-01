766ff07f34406b4dbf1412863c0b3124df7649d5
Dima Prosto <csourgg@gmail.com>
2024-06-01 23:46:59 +0300

    split into more functions and refactored the code according to the SOLID principle

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,21 +5,24 @@ const buttons = document.querySelectorAll('.btn');
 const blockFlexDirection = document.querySelector('.block-item')
 const items = document.querySelectorAll('.item');
 
+function maxWidthDependingDirection(direction) {
+    const maxWidth = (direction === 'column' || direction === 'column-reverse') ? '100%' : '30px';
+    items.forEach(item=> item.style.setProperty('max-width', maxWidth));
+    
+    blockFlexDirection.style.setProperty('flex-direction',`${direction}`);
+}
 
 function activeButtonClick(event) {
     buttons.forEach(button => button.classList.remove('active'));
     event.target.classList.add('active');
-    console.log(event.target.dataset.key);
-    blockFlexDirection.style.setProperty('flex-direction',`${event.target.dataset.key}`);
-    if (event.target.dataset.key === 'column' || event.target.dataset.key === 'column-reverse') {
-        items.forEach(item=> item.style.setProperty('max-width','100%'));
-    } else {
-        items.forEach(item=> item.style.setProperty('max-width','30px'));
-    }
+    const direction = event.target.dataset.key;
+
+    maxWidthDependingDirection(direction);
 }
+
+
 if (buttons.length > 0) {
     buttons[0].classList.add('active');
-    console.log(buttons[0].dataset.key);
 }
 
 buttons.forEach(button => {
