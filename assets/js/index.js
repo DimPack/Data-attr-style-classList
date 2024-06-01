3b5cfa9f48b5b90ad1907be6ecd96ee973f220fc
Dima Prosto <csourgg@gmail.com>
2024-06-01 23:19:50 +0300

    complete the task

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,20 @@
 
 
 const buttons = document.querySelectorAll('.btn');
+const blockFlexDirection = document.querySelector('.block-item')
+const items = document.querySelectorAll('.item');
 
 
 function activeButtonClick(event) {
     buttons.forEach(button => button.classList.remove('active'));
-    event.target.classList.add('active');  
+    event.target.classList.add('active');
+    console.log(event.target.dataset.key);
+    blockFlexDirection.style.setProperty('flex-direction',`${event.target.dataset.key}`);
+    if (event.target.dataset.key === 'column' || event.target.dataset.key === 'column-reverse') {
+        items.forEach(item=> item.style.setProperty('max-width','100%'));
+    } else {
+        items.forEach(item=> item.style.setProperty('max-width','30px'));
+    }
 }
 if (buttons.length > 0) {
     buttons[0].classList.add('active');
